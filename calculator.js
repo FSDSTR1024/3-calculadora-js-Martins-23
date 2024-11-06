cbbb43f16e2f59603d035b26d662aa0cb9969153
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-11-06 06:51:03 +0100

    Create the calculator class, instantiate an object and move the logic to a method of it

--- calculator.js
+++ calculator.js
@@ -1,28 +1,36 @@
+/*************************** CONSTANTS ***************************/
 // Save relevant HTML elements, for later usage
 const display = document.getElementById("display");
 const buttonsDiv = document.getElementById("buttons");
 
-// Function to add the input number to the display, handling 0's
-const _addNumberToDisplay = (number) => {
-    console.log(number);
-    // If the display is empty, set it to the introduced value
-    if (display.textContent.normalize('NFKC').includes('\u200B')) {
-        display.textContent = number;
-        return;
+/*********************** CALCULATOR CLASS ***********************/
+class Calculator {
+    // Function to add the input number to the display, handling 0's
+    addNumberToDisplay(number) {
+        // If the display is empty, set it to the introduced value
+        if (display.textContent.normalize('NFKC').includes('\u200B')) {
+            display.textContent = number;
+            return;
+        }
+        // If current value in display is 0, replace it with introduced value
+        if (display.textContent === "0") {
+            display.textContent = number;
+            return;
+        }
+        display.textContent += number.toString();
     }
-    // If current value in display is 0, replace it with introduced value
-    if (display.textContent === "0") {
-        display.textContent = number;
-        return;
-    }
-    display.textContent += number.toString();
 }
+
+/************************ WEB PAGE LOGIC ************************/
+// Create a new instance of the Calculator class
+const calculator = new Calculator();
 
 // Add to the number buttons the event listener to be written in the display
 for (let child of buttonsDiv.children) {
     if (child.classList.contains("number")) {
         child.addEventListener("click", () => {
-            _addNumberToDisplay(child.textContent);
+            const pressedButtonNumber = child.textContent;
+            calculator.addNumberToDisplay(pressedButtonNumber);
         });
     }
 }
